45c96bb00c7e316ff7ccded8b885a02630751716
MaxHiit <pro.maxcode@gmail.com>
2024-03-04 08:51:37 +0100

    feat(video): video capture package and video capture function

--- src/script.js
+++ src/script.js
@@ -2,14 +2,48 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import GUI from 'lil-gui';
 
+import { CCapture } from 'ccapture.js-npmfixed';
+
 import testVertexShader from './shaders/test/vertex.glsl';
 import testFragmentShader from './shaders/test/fragment.glsl';
 
 /**
  * Base
  */
+// Video Capture
+
+let isCapturing = false;
+let captureStartTime;
+
+// GUI controller
+const controller = {
+	startCapture: function () {
+		if (!isCapturing) {
+			capturer.start();
+			isCapturing = true;
+			captureStartTime = Date.now(); // reset the capture start time when starting capture
+		}
+	},
+	stopCapture: function () {
+		if (isCapturing) {
+			capturer.stop();
+			capturer.save();
+			isCapturing = false;
+		}
+	}
+};
+
+const capturer = new CCapture({
+	format: 'webm',
+	framerate: 60,
+	verbose: true
+});
+
 // Debug
 const gui = new GUI();
+
+gui.add(controller, 'startCapture').name('Start Capture');
+gui.add(controller, 'stopCapture').name('Stop Capture');
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -125,13 +159,18 @@ const tick = () => {
 	const elapsedTime = clock.getElapsedTime();
 
 	// update material
-	material.uniforms.uTime.value = elapsedTime;
+	material.uniforms.uTime.value = elapsedTime * 3;
 
 	// Update controls
 	controls.update();
 
 	// Render
 	renderer.render(scene, camera);
+
+	// Capture frame
+	if (isCapturing) {
+		capturer.capture(renderer.domElement);
+	}
 
 	// Call tick again on the next frame
 	window.requestAnimationFrame(tick);
